57080f8afb3791f6895f3f14119209449349a769
anibal Fermandois <anibalfermandois@uc.cl>
2025-06-28 13:36:46 -0400

    connected dots and image gallery start

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,29 +5,16 @@ export default {
     extend: {
       colors: {
         primary: {
-          50: '#f9ad06',
           100: '#f9ad06',
-          200: '#f9ad06',
-          300: '#f9ad06',
-          400: '#f9ad06',
-          500: '#f9ad06',
-          600: '#f9ad06',
-          700: '#f9ad06',
-          800: '#f9ad06',
-          900: '#f9ad06',
+          44: '#f7ac06',
         },
+        orangeNik: '#f76f2b',
         secondary: {
-          50: '#0b2a3f',
           100: '#0b2a3f',
-          200: '#0b2a3f',
-          300: '#0b2a3f',
-          400: '#0b2a3f',
-          500: '#0b2a3f',
-          600: '#0b2a3f',
-          700: '#0b2a3f',
-          800: '#0b2a3f',
-          900: '#0b2a3f',
-        }
+          80:'#0b2a3f'
+        },
+       
+        whiteNik:'#e6e6e6'
       },
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
